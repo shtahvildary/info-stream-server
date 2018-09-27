9129e69d80e06d3092c48906f38fd5464e7f831a
shtahvildary <shtahvildary@gmail.com>
2018-09-27 21:23:08 +0330

    change player source is ok

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ var server = http.createServer()
 
 var hls = new HLSServer(server, {
   path: '',     // Base URI to output HLS streams
-  dir: 'D:/hls-test'  // Directory that input files are stored
+  dir: '/Users/shadab/Downloads/hls-server-master/test/files/output'  // Directory that input files are stored
+  // dir: 'D:/hls-test'  // Directory that input files are stored
   // dir: 'public/videos'  // Directory that input files are stored
 })
 
