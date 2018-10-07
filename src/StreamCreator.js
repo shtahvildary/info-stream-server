6fd8f988285e3c25817edb0ed3e101e70a9238bd
shtahvildary <shtahvildary@gmail.com>
2018-10-07 15:41:43 +0330

    working on sdi input

--- src/StreamCreator.js
+++ src/StreamCreator.js
@@ -47,10 +47,51 @@ class StreamCreator {
       })
       .run();
   }
+
+  dShowFfmpeg(video, name) {
+    ffmpeg(address, { timeout: 432000 })
+      .addOptions([
+        // ffmpeg('/Users/shadab/Downloads/video_2017-08-09_18-26-12.mp4', { timeout: 432000 }).addOptions([
+
+
+        "-profile:v baseline", // baseline profile (level 3.0) for H264 video codec
+        "-level 3.0",
+        "-s 720x576", // 720px width, 576px height output video dimensions
+        "-start_number 0", // start the first .ts segment at index 0
+        "-hls_time 10", // 10 second segment duration
+        // '-hls_list_size 0',    // Maxmimum number of playlist entries (0 means all entries/infinite)
+        "-hls_flags delete_segments",
+        "-f dshow" // direct show format
+      ])
+      .output("/Users/shadab/Desktop/hls-test/" + name + ".m3u8")
+      // ]).output('/Users/shadab/Desktop/hls-test/out.m3u8').on('start',startCallback).on('end', endCallback).run()
+      // ]).output('d:/hls-test/'+name+'.m3u8')
+
+      .on("start", function() {
+        console.log(name + " is started :)");
+      })
+      .on("progress", function(progress) {
+        // console.log(name+ ': ... frames: ' + progress.frames);
+      })
+      .on("end", function() {
+        console.log(name + " has stoped :(");
+        request.post(
+          global.serverAddress+"/streamServer/hasChanged",
+          // "http://localhost:5000/api/streamServer/hasChanged",
+          { json: { name, address } },
+          (err, body, response) => {
+            //////////
+          }
+        );
+      })
+      .run();
+  }
   coding() {
     console.log(this.inputs);
     this.inputs.forEach(async (i, index) => {
+      if(i.dshow===0)
       this.callFfmpeg(i.address, i.name);
+      else this.dShowffmpeg(i.address,i.name)
     });
   }
 }
